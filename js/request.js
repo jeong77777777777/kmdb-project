fad988839316e998a2bd5995567fc381a8082a60
jeong77777777777 <fnsk4662@gmail.com>
2024-02-05 17:49:05 +0900

    240205

--- js/request.js
+++ js/request.js
@@ -1,3 +1,4 @@
+/* ====== Common GET Request Function ====== */
 async function getRequest(url) {
   return await fetch(url).then((response) => {
     if (!response.ok) {
@@ -7,27 +8,31 @@ async function getRequest(url) {
   });
 }
 
-async function getProducts() {
-  const url = `https://yts.mx/api/v2/list_movies.json?limit=10&order_by=desc`;
+async function getProducts(page) {
+  const url = `https://yts.mx/api/v2/list_movies.json?limit=5&order_by=desc&genre=Animation&page=${page}`;
 
   try {
-    // 데이터 요청 및 응답 시도 : 성공일 경우 첫번째 코드 블럭으로 이동
+    // 데이터 요처 및 응답 시도 : 성공일 경우 첫번째 코드 블럭으로 이동
     const data = await getRequest(url);
-    const movieWrapper = document.querySelector('.imgs');
-    // const movieTitle = document.querySelector('.title');
-    console.log(data.data.movies);
+    const randomData = Math.floor(Math.random() * 5); // 0보다 크고 1보다 작은 난수
+    //Math.floor : 소수점 이하를 버림
+    console.log(data.data.movies[randomData]);
+    const movieWrapper = document.querySelector('.recent-img');
+    const mainMovie = data.data.movies[randomData];
+    const imgEl = `<img src=${mainMovie.large_cover_image}>`;
 
-    const movies = data.data.movies;
-    movies.forEach((movie) => {
-      const imgEl = `<div class=imgbox><img src=${movie.medium_cover_image}><p class=title>${movie.title}</p></div>`;
-      // const titleEl = `${movie.title}`;
-      movieWrapper.insertAdjacentHTML('beforeend', imgEl);
-      // movieTitle.insertAdjacentHTML('beforeend', titleEl);
-    });
+    movieWrapper.insertAdjacentHTML('beforeend', imgEl);
   } catch (error) {
     // 실패할 경우 두번째 코드 블럭으로 이동
     console.log('Error : ', error);
   }
 }
 
-getProducts();
+getProducts(1);
+
+const numbers = document.querySelectorAll('.numbers span');
+numbers.forEach((number) => {
+  number.addEventListener('click', function () {
+    alert(this.textContent);
+  });
+});
